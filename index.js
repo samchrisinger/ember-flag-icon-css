825efa54a2ac37cc1803184a898dd22f4d5b7fe8
Sam Chrisinger <s.chrisinger@gmail.com>
2016-04-15 10:29:22 -0400

    Implement treeForPublic
    - Merge flags and css into public dir. This seems a litte bad, but
    flag-icon-css is written with implicit assumptions about the strucutre
    of static assets. For now, this works.

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var upstreamMergeTrees = require('broccoli-merge-trees');
+
+// from: https://github.com/ember-cli/ember-cli/blob/master/lib/models/addon.js#L25
+function mergeTrees(inputTree, options) {
+    options = options || {};
+    options.description = options.annotation;
+    var tree = upstreamMergeTrees(inputTree, options);
+
+    tree.description = options && options.description;
+
+    return tree;
+}
+
 module.exports = {
-  name: 'ember-flag-icon-css'
+    name: 'ember-flag-icon-css',
+    treeForPublic: function(tree) {
+        var baseTree = this._super.treeForPublic(...arguments);
+
+        var flagsFunnel = new Funnel(
+            this.app.trees.bower, {
+                srcDir: path.join('flag-icon-css', 'flags'),
+		destDir: path.join('assets', 'flags')
+            }
+        );
+	var cssFunnel = new Funnel(
+	    this.app.trees.bower,
+	    {
+		srcDir: path.join('flag-icon-css', 'css'),
+		destDir: path.join('assets', 'css')
+	    }
+	);
+	
+        return mergeTrees([flagsFunnel, cssFunnel, baseTree, tree].filter(Boolean));
+    }
 };
